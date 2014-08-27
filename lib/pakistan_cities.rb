6d045a8d077fa5c4ffbc956f95f3b061f0ebf142
Zunair Minhas <zunair.minhas@tkxel.com>
2014-08-27 18:32:27 +0500

    correct yaml path

--- lib/pakistan_cities.rb
+++ lib/pakistan_cities.rb
@@ -4,7 +4,7 @@ require "yaml"
 module PakistanCities
 
   def self.show_famous_cities
-    famous_cities = YAML.load_file('pakistan_cities.yml')
+    famous_cities = YAML::load_file(File.join(File.dirname(__FILE__), 'pakistan_cities.yml'))
     famous_cities['cities'].each do |city|
       puts city
     end
