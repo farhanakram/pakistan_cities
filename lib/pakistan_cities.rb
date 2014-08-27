6a071f0ced7103d374a8ea5fe7ce1cd19b0993c7
Zunair Minhas <zunair.minhas@tkxel.com>
2014-08-27 18:23:24 +0500

    change function name

--- lib/pakistan_cities.rb
+++ lib/pakistan_cities.rb
@@ -3,7 +3,7 @@ require "yaml"
 
 module PakistanCities
 
-  def show_famous_cities
+  def self.show_famous_cities
     famous_cities = YAML.load_file('pakistan_cities.yml')
     famous_cities['cities'].each do |city|
       puts city
