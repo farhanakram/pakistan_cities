918da6e29cdb2dce8c5bdf12fe15c6e2b0befc8e
Zunair Minhas <zunair.minhas@tkxel.com>
2014-08-28 11:40:15 +0500

    Organize methods

--- lib/pakistan_cities.rb
+++ lib/pakistan_cities.rb
@@ -2,11 +2,31 @@ require "pakistan_cities/version"
 require "yaml"
 
 module PakistanCities
+  @@all_cities = YAML::load_file(File.join(File.dirname(__FILE__), 'pakistan_cities.yml'))
 
-  def self.show_famous_cities
-    famous_cities = YAML::load_file(File.join(File.dirname(__FILE__), 'pakistan_cities.yml'))
-    
+  def self.get_balochistan_cities
+    get_province_cities("balochistan")
+  end
+
+  def self.get_kpk_cities
+    get_province_cities("kpk")
+  end
+
+  def self.get_punjab_cities
+    get_province_cities("punjab")
+  end
+
+  def self.get_sindh_cities
+    get_province_cities("sindh")
+  end
+
+  def self.get_province_cities(province)
+    cities = @@all_cities[province]
+  end
+
+  def self.get_all_cities
+    cities = @@all_cities
   end
 end
 
-PakistanCities::show_famous_cities
+PakistanCities::get_all_cities
